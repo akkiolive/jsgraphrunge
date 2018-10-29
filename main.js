d013c5d2c1aa4f722b729ae4cad4651504e57c08
taka <vancharfru@gmail.com>
2018-10-29 13:09:17 +0900

    lorenz attractor

--- main.js
+++ main.js
@@ -119,6 +119,21 @@
 			this.originingraph = new Point(undefined,undefined);
 			this.flags = new DrawFlags();
 			this.style = new DrawStyle();
+			this.original = this;
+		}
+
+		reset(){
+			//this = this.original;
+			this.location = this.original.location;
+			this.size = this.original.size;
+			this.range = this.original.range;
+			this.grid = this.original.grid;
+			this.dtl = new Point(undefined, undefined);
+			this.ltd = new Point(undefined, undefined);
+			this.data = [];
+			this.originingraph = new Point(undefined,undefined);
+			this.flags = new DrawFlags();
+			this.style = new DrawStyle();
 		}
 
 		setStyle(style){
@@ -128,6 +143,10 @@
 		renewRange(){
 			var mini = findmin(this.data);
 			var maxi = findmax(this.data);
+			if(this.data.length==0){
+				console.log("graph has no data so fail renewing");
+				return;
+			}
 			this.range = new Range(this.data[mini].x, this.data[mini].y, this.data[maxi].x, this.data[maxi].y);
 			this.dtl.x = this.size.x / this.range.xdistance;
 			this.dtl.y = this.size.y / this.range.ydistance;
@@ -144,7 +163,7 @@
 		}
 
 		addPoint(point){
-			console.log("add point -> ("+point.x+", "+point.y+")");
+			//console.log("add point -> ("+point.x+", "+point.y+")");
 			this.data.push(point);
 			this.renewRange();
 		}
@@ -171,6 +190,16 @@
 			return new Point(x,y);
 		}
 
+		l2d(point){
+			var x = (point.x - this.originingraph.x - this.location.x)/ this.dtl.x;
+			var y = (-point.y - this.originingraph.y + this.location.y)/ this.dtl.y;
+			return new Point(x,y);
+		}
+
+		delPrevPoint(){
+			delete this.data[this.data.length-1];
+		}
+
 		drawPoint(point){
 			var x = point.x * this.dtl.x + this.originingraph.x + this.location.x;
 			var y = -point.y * this.dtl.y + this.originingraph.y + this.location.y;
@@ -178,11 +207,11 @@
 			ctx.beginPath();
 			ctx.arc(x,y,3, 0, 2.0*Math.PI);
 			ctx.fill();
-			console.log("draw point -> ("+x+","+y+")");
+			//console.log("draw point -> ("+x+","+y+")");
 		}
 
 		drawLinePoint(data){
-			console.log("draw LinePoint");
+			//console.log("draw LinePoint");
 			ctx.fillStyle = "red";
 			ctx.beginPath();
 			var i = 0;
@@ -202,14 +231,14 @@
 		}
 
 		drawBorder(){
-			console.log("draw border");
+			//console.log("draw border");
 			ctx.strokeStyle = "black";
 			ctx.rect(this.location.x, this.location.y-this.size.y, this.size.x, this.size.y);
 			ctx.stroke();
 		}
 
 		drawGrid(){
-			console.log("draw grid");
+			//console.log("draw grid");
 			ctx.strokeStyle = "gray";
 			for(var i=0; i<10; i++){
 				var x = this.range.xmin + this.range.xdistance/10*i;
@@ -228,7 +257,7 @@
 		}
 
 		drawTics(xtics, ytics){
-			console.log("draw tics");
+			//console.log("draw tics");
 			for(var x=this.range.xmin; x<this.range.xmax; x+=xtics){
 				ctx.beginPath();
 				ctx.moveTo(x*this.dtl.x,0);
@@ -251,10 +280,8 @@
 	graph.drawTics(1,1);
 
 	class Param{
-		constructor(C, R, E){
-			this.C = C;
-			this.R = R;
-			this.E = E;
+		constructor(r){
+			this.r = r;
 		}
 	}
 
@@ -265,17 +292,23 @@
 			this.x = x0; //list
 			this.param = param;
 			this.dt = 0.05;
+			console.log(this.func);
 		}
 
 		func(n, t, x){
-			var C = this.param.C;
-			var R = this.param.R;
-			var E = this.param.E;
-			var dxdt;
+			var r = this.param.r;
+			var dxdt = [];
+			var xx = x[0];
+			var y = x[1];
+			var z = x[2];
+			console.log(xx);
 			if(n==0){
-				dxdt =(E-x[0]);
-				//dxdt = x[0];
-				//dxdt = 1.0;
+				dxdt[0] = -10*xx + 10*y;
+				dxdt[1] = -xx*z + r*xx - y;
+				dxdt[2] = xx*y - 8/3*z;
+				//dxdt[0] = xx + 3;
+				//dxdt[1] = xx;
+				//dxdt[2] = Math.sin(t*z);
 			}
 			else if(n==1){
 				dxdt = 0;
@@ -286,7 +319,7 @@
 			return dxdt;
 		}
 
-		runge(t, x){ //proceed 1 step
+		runge(n){ //proceed 1 step
 			var k1 = [];
 			var k2 = [];
 			var k3 = [];
@@ -294,38 +327,96 @@
 			var k = [];
 			var dt = this.dt;
 			var tmpx = this.x;
+			k1 = this.func(n, this.t, tmpx);
 			for(var i = 0; i<this.N; i++) {
-				k1[i] = dt*this.func(i, this.t, tmpx);
-				tmpx[i] += k1[i]/2.0;
+				tmpx[i] += dt*k1[i]/2.0;
 			}
+			k2 = this.func(n, this.t+dt/2.0, tmpx);
 			for(var i = 0; i<this.N; i++) {
-				k2[i] = dt*this.func(i, this.t+dt/2.0, tmpx);
-				tmpx[i] += k2[i]/2.0
+				tmpx[i] += dt*k2[i]/2.0
 			}
+			k3 = this.func(n, this.t+dt/2.0, tmpx);
 			for(var i = 0; i<this.N; i++) {
-				k3[i] = dt*this.func(i, this.t+dt/2.0, tmpx);
-				tmpx[i] += k3[i];
+				tmpx[i] += dt*k3[i];
 			}
-			for(var i = 0; i<this.N; i++) {
-				k4[i] = dt*this.func(i, this.t+dt, tmpx);
-			}
-			for(var i = 0; i<this.N; i++) k[i] = (k1[i] + 2.0*(k2[i]+k3[i]) + k3[i]) / 6.0;
+			k4 = this.func(n, this.t+dt, tmpx);
+			for(var i = 0; i<this.N; i++) k[i] = dt*(k1[i] + 2.0*(k2[i]+k3[i]) + k3[i]) / 6.0;
 			for(var i = 0; i<this.N; i++) this.x[i] += k[i];
 			this.t += dt;
 		}
 		
+		read(){
+			if(document.formf.inputf.value==""){
+ 				//document.getElementById("message").innerText = "no func!";
+ 				console.log("no func!");
+ 				return ;
+			}
+			var tf = function(n, t, x){
+				var strf = document.formf.inputf.value;
+				var strg = document.formf.inputg.value;
+				var strh = document.formf.inputh.value;
+				var dxdt = [];
+				dxdt[0] = eval(strf);
+				dxdt[1] = eval(strg);
+				dxdt[2] = eval(strh);
+				return dxdt;
+			}	
+			var xxx = [];
+			for(var i=0; i<this.N; i++){
+				xxx.push(i);
+			}
+			var test = tf(this.N, 0.05, xxx);
+			console.log("test="+test);
+			if(test==undefined) return	;
+			this.func = tf;
+			//console.log(this.x);
+			//var val = this.func(0, 0, this.x);
+			//console.log(val);
+			console.log(this.func);
+		}
+
+		readparam(){
+			console.log("read param!");
+			var paramsname = [];
+			var params = [];
+			for(var i=0; i<document.formparam.elements.length/2-1; i++){
+				paramsname[i] = document.formparam.elements[i*2].value;
+				params[i] = Number(document.formparam.elements[i*2+1].value);
+				if(paramsname[i]=="")continue;
+				console.log(paramsname[i]+"="+params[i]);
+				eval("this."+paramsname[i]+"="+params[i]);
+			}
+		}
+
 	}
 
-	var x0 = [0.1];
-	var param = new Param(0.47e-6, 100000.0, 15);
-	var dyn = new Dynanmics(1, x0, param);
+	var x0 = [1, 1, 1];
+	var param = new Param(4); //r=28; diverged
+	var dyn = new Dynanmics(3, x0, param);
+	//	dyn.read();
 	for(var i=0; i<50; i++){
-		dyn.runge();
-		graph.addPoint(new Point(dyn.t, dyn.x[0]));
+		dyn.runge(0);
+		if(dyn.x[1]!=NaN)graph.addPoint(new Point(dyn.x[0], dyn.x[1]));
+		if(dyn.x[1]<0)dyn.x[1] = -dyn.x[1];
+		console.log(new Point(dyn.x[0], dyn.x[1]));
 	}
 	graph.drawLinePoint(graph.data);
 	console.log(graph);
 
+
+ 	stage.onclick = function(e) {
+	    // 一度描画をクリア
+	   // ctx.clearRect(0, 0, canvasW, canvasH);
+
+	    // クリック位置の座標計算（canvasの左上を基準。-2ずつしているのはborderの分）
+	    var rect = e.target.getBoundingClientRect();
+	    var mouseX = e.clientX - Math.floor(rect.left) - 2;
+	    var mouseY = e.clientY - Math.floor(rect.top) - 2;
+	    var xyp = new Point(mouseX, mouseY);
+	    var xy = graph.l2d(xyp);
+	    console.log(xy);
+	    graph.addPoint(xy);
+	}
 
 	function keydown(event) {
 		//inhibit scrolling
@@ -353,6 +444,11 @@
 
 		}
 
+		if(event.keycode == 66) { //b key
+			console.log("del prev Point");
+			graph.delPrevPoint();
+		}
+
 		//draw
 		redraw();
 		
@@ -369,10 +465,9 @@
 		ctx.clearRect(0,0,width,height); //clear
 		graph.style="lp";
 		graph.draw();
-
 	}
 
-	var animateflag = 0
+	var animateflag = 1;
 	function animate(){
 		if(!animateflag) return;
 
@@ -380,7 +475,60 @@
 	}
 
 	redraw();
-	setInterval(animate, 80);
+	setInterval(animate, 300);
+
+	var button = document.getElementById('inputfb');
+	button.addEventListener('click', dyn.read);
+	var buttonp = document.getElementById('inputparamb');
+	buttonp.addEventListener('click', function(){
+		dyn.readparam();
+	});
+
+	document.getElementById('drawb').addEventListener('click', function(){
+		graph.draw();
+	});
+
+	document.getElementById('adddatab').addEventListener('click', function(){
+		for(var i=0; i<50; i++){
+			dyn.runge();
+			graph.addPoint(new Point(dyn.t, dyn.x[0]));
+		}
+		graph.drawLinePoint(graph.data);
+	});
+
+	document.getElementById('resetb').addEventListener('click', function(){
+		graph.reset();
+		graph.draw();
+	});
+
+	document.getElementById('clearanddrawb').addEventListener('click', function(){
+		dyn.read();
+		graph.reset();
+		console.log(dyn.func);
+		dyn.t = 0;
+		dyn.x = x0;
+		for(var i=0; i<50; i++){
+			dyn.runge();
+			graph.addPoint(new Point(dyn.x[0], dyn.x[1]));
+		}
+		graph.drawLinePoint(graph.data);
+		graph.draw();
+	});
+
+	document.getElementById('inputfid').addEventListener('input', function(){
+		/*
+		dyn.read();
+		graph.reset();
+		console.log(dyn.func);
+		dyn.t = 0;
+		for(var i=0; i<50; i++){
+			dyn.runge();
+			graph.addPoint(new Point(dyn.t, dyn.x[0]));
+		}
+		graph.drawLinePoint(graph.data);
+		graph.draw();
+		*/
+	});
 
 
 		
